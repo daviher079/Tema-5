e8ab451a581e2b9eff6e2e8dded6cc6641046524
david <daviher079@gmail.com>
2021-12-29 13:39:22 +0000

    CambiosTeam5

---
+++ EjemploHtAccess/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../web-root/css/resetCSS.css"/>
+    <link rel="stylesheet" href="../web-root/css/style.css"/>
+    <title>Ejemplo .htaccess</title>
+</head>
+<body>
+    <header>
+        <img class="logo" src="../web-root/img/LogotipoDavid.jpg"/>
+        <h1>Ejemplo .htaccess</h1>
+    </header>
+    <main>
+        <h2>Desea entrar en:</h2>
+        <div style="display: block;">
+            <a href="./administrador/config.php">Administrador</a>
+            <a href="./usuarios/perfil.php">Usuario</a>
+
+        </div>
+    </main>
+    <footer>
+        <p>Footer de David</p>
+        <a href="../index.html"><img src="../web-root/img/volver.png" height="20px"></a>
+    </footer>
+</body>
+</html>
